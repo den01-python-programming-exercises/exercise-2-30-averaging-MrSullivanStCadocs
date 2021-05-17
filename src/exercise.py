8b45cedc615a8d1df539c2fd7b2dd432c4a26c35
MrSullivan <>
2021-05-17 22:07:16 +0000

    three methods and average with two return calls

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    result = average(4, 3, 6, 1)
+    print("Average: " + str(result))
+
+def sum(number1, number2, number3, number4):
+  return number1 + number2 + number3 + number4
+
+def average(number1, number2, number3, number4):
+  return sum(number1, number2, number3, number4) / 4
 
 if __name__ == '__main__':
     main()
